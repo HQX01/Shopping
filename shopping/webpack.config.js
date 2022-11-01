dfc0cadc015b7e4012e49220de0a35d31286eccc
HQX01 <hqx666@bupt.edu.cn>
2022-11-01 12:12:40 +0800

    feat:add webpack.config.js

---
+++ shopping/webpack.config.js
@@ -0,0 +1,47 @@
+// ./webpack.config.js
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: ['./src/index.js', './src/App.js'],
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.svg/,
+                type: 'asset/inline',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html',
+            inject: true,
+            minify: true,
+        }),
+        new MiniCssExtractPlugin({
+            filename: './css/index.css',
+        }),
+    ],
+};
