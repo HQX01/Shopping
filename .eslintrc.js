afe1d723ad0e64e9619de8d245513c16ea1f2348
HQX01 <hqx666@bupt.edu.cn>
2022-11-08 10:28:27 +0800

    feat: init git hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,13 @@
 module.exports = {
-    extends: ["react-app"], //继承react官方规则
-    parserOptions: {
-        babelOptions: {
-            presets: [
-                //解决页面报错问题
-                ["babel-preset-react-app", false],
-                "babel-preset-react-app/prod",
-            ],
-        },
+  extends: ["react-app"], //继承react官方规则
+  parserOptions: {
+    babelOptions: {
+      presets: [
+        //解决页面报错问题
+        ["babel-preset-react-app", false],
+        "babel-preset-react-app/prod",
+      ],
     },
+  },
+  ignorePatterns: ["node_modules/*", "dist/*", "*.scss", "*.json", "cypress/*"],
 };
--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
-import './App.css';
+import "./App.css";
 import React, { Suspense, lazy } from "react";
 import { Link, Routes, Route } from "react-router-dom";
 //import HomePage from "./pages/homePage";
 //import ShoppingCartPage from "./pages/shoppingCartPage";
-import logo from './logo.svg';
-import bigpicture from './bigpicture.png'
-import "./iconfont.css"
+import logo from "./logo.svg";
+import bigpicture from "./bigpicture.png";
+import "./iconfont.css";
 
-const Home = lazy(() => import(/*webpackChunkName: 'home' */  "./pages/homePage")) ;
-const ShoppingCart = lazy(() => import(/*webpackChunkName: 'shoppingcart' */  "./pages/shoppingCartPage"));
+const Home = lazy(() =>
+  import(/*webpackChunkName: 'home' */ "./pages/homePage")
+);
+const ShoppingCart = lazy(() =>
+  import(/*webpackChunkName: 'shoppingcart' */ "./pages/shoppingCartPage")
+);
 
 function App() {
   return (
     <div className="App">
-        <span className="iconfont icon-add-cart"></span>
-        <img src={logo} className="App-logo" alt="logo" />
+      <span className="iconfont icon-add-cart"></span>
+      <img src={logo} className="App-logo" alt="logo" />
       <ul>
-          <li>
-              <Link to="/home">Home</Link>
-          </li>
-          <li>
-              <Link to="/shoppingcart">ShoppingCart</Link>
-          </li>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/shoppingcart">ShoppingCart</Link>
+        </li>
       </ul>
-        <img src={bigpicture} className="big-picture" alt="bigpicture" />
-       <Suspense fallback={<div>loading...</div>}>
-           <Routes>
-               <Route path="/home" element={<Home />} />
-               <Route path="/shoppingcart" element={<ShoppingCart />} />
-           </Routes>
-       </Suspense>
+      <img src={bigpicture} className="big-picture" alt="bigpicture" />
+      <Suspense fallback={<div>loading...</div>}>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/shoppingcart" element={<ShoppingCart />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
