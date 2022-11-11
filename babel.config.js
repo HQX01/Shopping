ad1ba33ae923fb247351b0b914cd73e358d1215b
HQX01 <hqx666@bupt.edu.cn>
2022-11-11 10:37:48 +0800

    feat: fix jest

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     //https://github.com/facebook/create-react-app/blob/main/packages/babel-preset-react-app/create.js
     //babel-preset-react-app
-    presets: ["react-app"],
-
+    presets: [
+        "react-app",
+    ],
+    
 };
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import HomePage from './pages/homePage/index';
+import { jest } from '@jest/globals';
 
 test('renders learn react link', () => {
   render(<HomePage />);
