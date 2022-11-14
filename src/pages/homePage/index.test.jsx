258739c5b5dbb42afe1f69b3654c8eed8c410c7c
HQX01 <hqx666@bupt.edu.cn>
2022-11-14 10:50:43 +0800

    feat: add some unit tests

---
+++ src/pages/homePage/index.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import HomePage from './index';
+import { jest } from '@jest/globals';
+
+test('renders learn react link', () => {
+    render(<HomePage />);
+    expect(screen.getByText('Home')).toBeInTheDocument();
+});
---
+++ src/pages/shoppingCartPage/index.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import ShoppingCartPage from './index';
+import { jest } from '@jest/globals';
+
+test('renders learn react link', () => {
+    render(<ShoppingCartPage />);
+    expect(screen.getByText('ShoppingCart')).toBeInTheDocument();
+});
